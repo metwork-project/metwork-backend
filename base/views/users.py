0654079d67115bbab67a80e0b1ffac5a10fa7c90
YannBeauxis <pro@yannbeauxis.net>
2018-09-05 14:13:26 +0200

    allow register

--- base/views/users.py
+++ base/views/users.py
@@ -30,7 +30,7 @@ def betatest_register(request):
         payload = json.loads(request.body)
     except ValueError:
         return JsonResponse({"error": "Unable to parse request body"}, status=400)
- 
+
     form = BetaTestRegistrationForm(payload)
 
     if form.is_valid():
@@ -39,7 +39,7 @@ def betatest_register(request):
             'name': form.cleaned_data["name"],
             'organization': form.cleaned_data["organization"]})
         return JsonResponse({"success": "User registered for betatest."}, status=201)
- 
+
     return HttpResponse(form.errors.as_json(), status=400, content_type="application/json")
 
 @require_http_methods(["POST"])
@@ -52,22 +52,27 @@ def register(request):
         payload = json.loads(request.body)
     except ValueError:
         return JsonResponse({"error": "Unable to parse request body"}, status=400)
- 
+
     form = RegistrationForm(payload)
 
     if form.is_valid():
+        username = form.cleaned_data["username"]
         user = get_user_model().objects.create_user(
-                                        email = form.cleaned_data["email"],
-                                        organization = form.cleaned_data["organization"],
-                                        password = form.cleaned_data["password"])
+                email = form.cleaned_data["email"],
+                username = username,
+                organization = form.cleaned_data["organization"],
+                password = form.cleaned_data["password"])
         user.save()
- 
+
+        #try:
         user.email_user(
-            subject = 'Metwork account created', 
-            message = 'Welcome to Metwork !')
+            subject = 'Metwork account created',
+            message = 'Welcome to Metwork {0}!\n\nYour account has been created.'.format(username))
+        #except:
+        #    pass
 
         return JsonResponse({"success": "User registered."}, status=201)
- 
+
     return HttpResponse(form.errors.as_json(), status=400, content_type="application/json")
 
 @require_http_methods(["POST"])
@@ -87,16 +92,16 @@ def password_reset(request):
     if form.is_valid():
         user = get_user_model()\
                 .objects.get(email = form.cleaned_data["email"])
- 
+
         temp_pwd = get_user_model().objects.make_random_password(length=10)
 
         user.set_password(temp_pwd)
         user.save()
 
         user.email_user(
-            subject = 'MetWork beta test password', 
+            subject = 'MetWork beta test password',
             message = 'Welcome to MetWork {0} !\n\nYou can now begin beta test on https://metwork.pharmacie.parisdescartes.fr\n\nYour email for login is : {1}\nYour password : {2}\n\nHave fun !\nThe MetWork Team'.format(user.first_name, user.email, temp_pwd))
 
         return JsonResponse({"success": "Reset paswword email sent"}, status=201)
- 
+
     return HttpResponse(form.errors.as_json(), status=400, content_type="application/json")
