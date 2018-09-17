ce784710efa4c94d06a213bb5c06146dbd57cd25
YannBeauxis <pro@yannbeauxis.net>
2018-09-17 02:06:57 +0200

    query atom and mapping

--- base/modules/chem_doodle.py
+++ base/modules/chem_doodle.py
@@ -19,77 +19,80 @@ class ChemDoodle(object):
 
     def json_to_mol(self, json):
         from base.models import Molecule
+        return Molecule.load_from_rdkit( self.json_to_rdkit(json) )
 
-        m0 = Chem.MolFromSmiles('')
+    def json_to_rdkit(self, json, map={}, mol_type='reactant'):
+        from base.models import Molecule
+
+        m0 = Chem.MolFromSmarts('')
         mw = Chem.RWMol(m0)
 
         atoms_cd = {}
-        bonds_cd = {}
-
+        query_atoms = []
 
         for atom in json['a']:
-            symbol = 'C' if 'l' not in atom else atom['l']
-            atoms_cd[atom['i']] = mw.AddAtom(Chem.Atom(symbol))
+            atom_id = atom['i']
+            # Query atoms
+            if 'q' in atom:
+                v = atom['q']['as']['v']
+                if 'a' in v:
+                    smarts = '[*]'
+                    a_defaut = 'C'
+                else:
+                    smarts = '[{0}]'.format(','.join([a_ for a_ in v ]))
+                    a_defaut = v[0]
+                query_atoms.append((atom_id,smarts))
+                a = Chem.Atom(a_defaut)
+            # Non-query atoms
+            else:
+                symbol = 'C' if 'l' not in atom else atom['l']
+                a = Chem.Atom(symbol)
+            atoms_cd[atom_id] = mw.AddAtom(a)
         for bond in json['b']:
             bond_id = bond['i']
             if 'o' in bond:
                 bond_type = self.bond_type(bond['o'])
             else:
                 bond_type = self.bond_type(1)
-            bonds_cd[bond_id] = bond
-            bonds_cd[bond_id]['rd_id'] = mw.AddBond(bond['b'], bond['e'],bond_type) - 1
+            mw.AddBond(bond['b'], bond['e'],bond_type) - 1
+
+        bonds_cd1 = {
+            i: mw.GetBondBetweenAtoms(b_cd['b'],b_cd['e']).GetIdx()
+            for i,b_cd in enumerate(json['b'])
+        }
+
+        bonds_cd = {
+            '{0}-{1}'.format(b_cd['b'], b_cd['e']): i
+            for i,b_cd in enumerate(json['b'])
+        }
 
         atoms_rd = {v: k for k, v in atoms_cd.items()}
-        bonds_rd = {v['rd_id']: k for k, v in bonds_cd.items()}
-
-        mol = mw.GetMol()
-        Chem.rdmolops.SanitizeMol(mol)
-        mol = Chem.MolFromSmiles(Chem.MolToSmiles(mol))
-
-        # for a in mol.GetAtoms():
-        #     a.SetAtomMapNum(a.GetIdx())
+        bonds_rd = {v: k for k, v in bonds_cd.items()}
 
         def a_point(atom_rd):
             x, y = ( json['a'][atom_rd][d] for d in ['x','y'] )
             return Point( x,y )
 
         def mol_atom(idx):
-            return mol.GetAtoms()[idx]
-
-    ### Manage stereochemistry
-
-        for bond in mol.GetBonds():
-            if bond.GetBondType() == Chem.rdchem.BondType.DOUBLE:
-                carbons = [ bond.GetBeginAtomIdx(), bond.GetEndAtomIdx() ]
-                # Check if stero can be applied
-                if True in [ len(mol_atom(c).GetBonds()) > 1 for c in carbons ]:
-
-                    sa = [ \
-                            [b.GetBeginAtomIdx() \
-                                if b.GetBeginAtomIdx() != c \
-                                else b.GetEndAtomIdx()#,
-                                for b in mol_atom(c).GetBonds() \
-                                    if b.GetBondType() == Chem.rdchem.BondType.SINGLE ] \
-                            for c in carbons ]
-
-                    sa = [ sa[i][0] for i in range(2) ]
-                    bond.SetStereoAtoms (sa[0], sa[1])
-
-                    if len( intersection(
-                            Segment( a_point(sa[0]), a_point(sa[1]) ),
-                            Line( a_point(carbons[0]), a_point(carbons[1]) )
-                        )) > 0:
-                        bond.SetStereo(Chem.rdchem.BondStereo.STEREOTRANS)
-                    else:
-                        bond.SetStereo(Chem.rdchem.BondStereo.STEREOCIS)
+            return mw.GetAtoms()[idx]
 
     ### Manage chirality
-        for atom in mol.GetAtoms():
+
+        for atom in mw.GetAtoms():
             if atom.GetSymbol() == 'C':
                 s = []
                 for b in atom.GetBonds():
-                    if 's' in bonds_cd[bonds_rd[b.GetIdx()]]:
-                        s.append(bonds_cd[bonds_rd[b.GetIdx()]]['s'])
+                    id_1 = '{0}-{1}'.format(
+                                        b.GetBeginAtomIdx(),
+                                        b.GetEndAtomIdx() )
+                    if id_1 in bonds_cd:
+                        b_cd = json['b'][ bonds_cd[id_1] ]
+                    else:
+                        b_cd = json['b'][ bonds_cd['{1}-{0}'.format(
+                                            b.GetBeginAtomIdx(),
+                                            b.GetEndAtomIdx() )] ]
+                    if 's' in b_cd:
+                        s.append(b_cd['s'])
                     else:
                         s.append('')
                 if len(s) == 4 and s.count('protruding')*s.count('recessed') == 1:
@@ -123,6 +126,56 @@ class ChemDoodle(object):
                     else:
                         atom.SetChiralTag(Chem.rdchem.ChiralType.CHI_TETRAHEDRAL_CW)
 
-        # print(Chem.MolToSmiles(mol))
+    ### Manage stereo
 
-        return Molecule.load_from_rdkit(mol)
+        for bond in mw.GetBonds():
+            if bond.GetBondType() == Chem.rdchem.BondType.DOUBLE:
+                carbons = [ bond.GetBeginAtomIdx(), bond.GetEndAtomIdx() ]
+                # Check if stero can be applied
+                if True in [ len(mol_atom(c).GetBonds()) > 1 for c in carbons ]:
+                    sa = [ \
+                            [b.GetBeginAtomIdx() \
+                                if b.GetBeginAtomIdx() != c \
+                                else b.GetEndAtomIdx()#,
+                                for b in mol_atom(c).GetBonds() \
+                                    if b.GetBondType() == Chem.rdchem.BondType.SINGLE ] \
+                            for c in carbons ]
+
+                    sa = [ sa[i][0] for i in range(2) ]
+                    bond.SetStereoAtoms (sa[0], sa[1])
+
+                    if len( intersection(
+                            Segment( a_point(sa[0]), a_point(sa[1]) ),
+                            Line( a_point(carbons[0]), a_point(carbons[1]) )
+                        )) > 0:
+                        bond.SetStereo(Chem.rdchem.BondStereo.STEREOTRANS)
+                    else:
+                        bond.SetStereo(Chem.rdchem.BondStereo.STEREOCIS)
+
+    # replace query_atoms
+
+        if len(query_atoms) > 0:
+            for id, smarts in query_atoms:
+                m_a = Chem.MolFromSmarts(smarts)
+                Chem.SanitizeMol(m_a)
+                mw.ReplaceAtom(atoms_cd[id],m_a.GetAtoms()[0])
+
+    # Map atoms
+        dic_type = {
+            'reactant': 'a1',
+            'product': 'a2' }
+        i = 1
+        for m in map:
+            if m['t'] == 'AtomMapping':
+                a_rd_id = atoms_cd[ m[ dic_type[mol_type] ] ]
+                mw.GetAtoms()[a_rd_id].SetAtomMapNum(i)
+                i += 1
+
+
+        mol = mw.GetMol()
+        Chem.rdmolops.SanitizeMol(mol)
+
+        # To keep stereo in MolToSmiles
+        Chem.rdDepictor.Compute2DCoords(mol)
+
+        return mol
--- base/tests/test_chemdoodle.py
+++ base/tests/test_chemdoodle.py
@@ -3,12 +3,15 @@ from __future__ import unicode_literals
 from django.test import TransactionTestCase
 from base.modules import ChemDoodle
 import json
+from rdkit import Chem
 
 class ChemDoodleTests(TransactionTestCase):
 
     def test_json_to_mol(self):
         # smiles = 'C/C=C/[C@@](C)(N)c1ccc(O)cc1'
         smiles = 'C/C(F)=C/[C@@](C)(N)c1ccc(O)cc1'
+        # smiles = 'C[C@@](N)(/C=C(\F)[NH2])c1ccc([*])cc1'
+
         json_path = 'base/tests/files/chemdoodle_mol_1.json'
         cd  = ChemDoodle()
         with open(json_path, 'r') as fjson:
@@ -16,3 +19,14 @@ class ChemDoodleTests(TransactionTestCase):
             json_mol = json.loads(json_str)[0]['m'][0]
 
         self.assertEqual(cd.json_to_mol(json_mol).smiles(),smiles)
+
+        smarts = '[N,O:1]/[#6](-[#9])=[#6]/[#6@@](-[#6])(-[#7])-[#6]1:[#6]:[#6]:[#6](-[*:2]):[#6]:[#6]:1'
+        json_path = 'base/tests/files/chemdoodle_mol_2.json'
+        cd  = ChemDoodle()
+        with open(json_path, 'r') as fjson:
+            json_str = '[{0}]'.format(fjson.readline())
+            json_data = json.loads(json_str)
+            json_mol = json_data[0]['m'][0]
+            json_map = json_data[0]['s']
+        mol = cd.json_to_rdkit(json_mol, map=json_map )
+        self.assertEqual(Chem.MolToSmarts(mol),smarts)
