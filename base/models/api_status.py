4557abc22f5a6bffc8ba880dbcda669b56847e50
YannBeauxis <pro@yannbeauxis.net>
2018-11-08 17:37:12 +0100

    cache molecules count in api status

--- base/models/api_status.py
+++ base/models/api_status.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals
 from rest_framework import serializers
 from base.models import Molecule, SampleAnnotationProject
 from django.db import models
+from django.core.cache import cache
 
 class APIStatus(models.Model):
 
@@ -14,7 +15,11 @@ class APIStatus(models.Model):
         return True
 
     def molecules_count(self):
-        return Molecule.objects.count()
+        count = cache.get_or_set(
+            'molecules_all_count',
+            Molecule.objects.count(),
+            60 )
+        return count
 
     def achieved_projects_count(self):
         return SampleAnnotationProject.achieved_projects().count()
