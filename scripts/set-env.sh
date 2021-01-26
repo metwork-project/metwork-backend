2981bf07b9e4806165aa038eeb6202ce6f5b3667
Yann Beauxis <dev@yannbeauxis.net>
2021-01-26 18:58:05 +0100

    .env METWORK_DATA_PATH

--- scripts/set-env.sh
+++ scripts/set-env.sh
@@ -3,6 +3,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 DIR="$( cd $DIR/.. >/dev/null 2>&1 && pwd )"
 ENV_DIR=$DIR/envs
 
+. $DIR/.env
 export METWORK_BACKEND_PATH=$DIR
 export METWORK_CONFIG=$ENV_DIR/common.env,$ENV_DIR/local.env
 
